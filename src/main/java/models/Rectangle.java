e454393e3fcab9f0d16e77781004142a0521bcf3
imukunya <64461160+imukunya@users.noreply.github.com>
2020-12-16 18:01:49 +0300

    v-1

---
+++ src/main/java/models/Rectangle.java
@@ -0,0 +1,27 @@
+package models;
+
+
+public class Rectangle {
+
+    private int height;
+    private int width;
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight(){
+        return this.height;
+    }
+
+    public int getLength() {
+        return 2;
+    }
+    public int getWidth() {
+        return this.width;
+    }
+    public boolean isSquare() {
+        return height == width;
+    }
+}
